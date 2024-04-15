6c9694a30358b108410e7ba00d72e18cc6da05a1
Natanael Signorini <natanaelsignorini01@gmail.com>
2024-04-15 17:35:20 -0300

    Revert ":sparkles: new module users"
    This reverts commit d5567f888aee37941f04bbf6c04ae7d995dac0de.

--- src/modules/users/dto/update-user.input.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { IsUUID } from 'class-validator';
-import { CreateUserInput } from './create-user.input';
-
-export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @IsUUID()
-  id?: string;
-}
