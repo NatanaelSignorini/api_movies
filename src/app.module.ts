b7509b8dc081dfc6da55671b81961a5685ef9a21
Natanael Signorini <natanaelsignorini01@gmail.com>
2024-05-16 20:23:42 -0300

    :wrench: update typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataBaseConfig } from './config/dataBaseConfig';
 import { AuthModule } from './modules/auth/auth.module';
 import { MoviesModule } from './modules/movies/movies.module';
 import { UsersModule } from './modules/users/users.module';
@@ -9,9 +8,15 @@ import { UsersModule } from './modules/users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useClass: DataBaseConfig,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 5432),
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
     }),
     // CacheModule.registerAsync(RedisOptions),
     AuthModule,
